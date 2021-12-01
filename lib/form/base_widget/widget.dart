ecd6b65beea1a37b298a26c522fdb2340d8ea63a
cht <3237126351@qq.com>
2021-12-02 00:18:38 +0800

    add two abstract class

--- lib/form/base_widget/widget.dart
+++ lib/form/base_widget/widget.dart
@@ -3,3 +3,11 @@ import 'package:flutter/material.dart';
 abstract class BaseWidget implements Widget{
 	int? score();
 }
+abstract class FromUrl {
+	String geturl();
+//	UrlBase urltype();
+}
+//enum UrlBase{
+//	videourl,
+//	texturl,
+//}
--- lib/form/papers.dart
+++ lib/form/papers.dart
@@ -4,8 +4,10 @@ import 'package:flutter/material.dart';
 typedef VideoPaperKey = GlobalKey<_VideoPaperState>;
 class VideoPaper extends StatefulWidget implements BaseWidget {
 	late final VideoPaperKey _key;
-	VideoPaper({required VideoPaperKey key}) : super(key:key){
+	late final String _url;
+	VideoPaper({required VideoPaperKey key,required String url}) : super(key:key){
 		_key = key;
+		_url = url;
 	}
 	@override
 	int? score() {
@@ -16,16 +18,32 @@ class VideoPaper extends StatefulWidget implements BaseWidget {
 }
 class _VideoPaperState extends State<VideoPaper> {
 	int a = 2;
+	late final String url;
+	@override
+  void initState() {
+    super.initState();
+		url = widget._url;
+  }
 	@override
 	Widget build(BuildContext context) {
-		return const Text("ssss");
+		return Text(url);
 	}
+}
+class VideoUrl implements FromUrl {
+	final String url;
+	const VideoUrl(this.url);
+	@override
+	String geturl() {
+		return url;
+  }
 }
 typedef TextPaperKey = GlobalKey<_TextPaperState>;
 class TextPaper extends StatefulWidget implements BaseWidget {
 	late final TextPaperKey _key;
-	TextPaper({required TextPaperKey key}): super(key:key){
+	late final String _url;
+	TextPaper({required TextPaperKey key,required String url}): super(key:key){
 		_key = key;
+		_url = url;
 	}
 	@override
 	int? score() {
@@ -36,8 +54,23 @@ class TextPaper extends StatefulWidget implements BaseWidget {
 	State<TextPaper> createState() => _TextPaperState();
 }
 class _TextPaperState extends State<TextPaper> {
+	late final String url;
+	@override
+  void initState() {
+    super.initState();
+		url = widget._url;
+
+  }
 	@override
 	Widget build(BuildContext context) {
-		return const Text('ssssssss');
+		return Text(url);
 	}
+}
+class TextUrl implements FromUrl {
+	final String url;
+	const TextUrl(this.url);
+	@override
+  String geturl() {
+		return url;
+  }
 }
